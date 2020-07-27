675c8a8a4d40fbeecbbe7348b7efa6f2e604a37d
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-07-27 08:06:17 +0000

    admin page is fixed

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
   def show
+    products = Product.all
+    @quantity = 0
+    products.each do |product|
+      @quantity += product.quantity
+    end
+    
+    @categories = Category.all
   end
 end
